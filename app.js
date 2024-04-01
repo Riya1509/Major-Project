71f73f13f65b3aac4b162929960f1405df3acf9d
Riya1509 <riya.m1509@gmail.com>
2024-04-01 21:00:48 +0530

    Adding new Changes in some files

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const app= express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-=======
+
 if(process.env.NODE_ENV != "production"){
   require('dotenv').config();
 }
@@ -12,19 +12,13 @@ if(process.env.NODE_ENV != "production"){
 console.log(process.env.SECRET);
 
 const express = require("express");
-const app= express();
+
 const mongoose = require("mongoose");
->>>>>>> c2fec04 (Added my project)
+ 
 const path = require("path");
 const methodOverride= require("method-override");
 const ejsMate = require("ejs-mate");
 const { appendFile } = require("fs");
-<<<<<<< HEAD
-
-
-
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-=======
 const ExpressError= require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -41,7 +35,7 @@ const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
->>>>>>> c2fec04 (Added my project)
+
 
 main().then(() =>{
     console.log("connected to DB");
@@ -52,18 +46,15 @@ main().then(() =>{
 
 
 async function main(){
-<<<<<<< HEAD
-    await mongoose.connect(MONGO_URL);
-    
-}
-=======
+
+  
   await  mongoose.connect(dbUrl);
+};
     
     
   
     
-};
->>>>>>> c2fec04 (Added my project)
+ 
 
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname, "views"));
@@ -72,91 +63,9 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-    res.send("Hi I am root");
-});
-
-// index route
-app.get("/listings", async (req, res) => {
-  const allListings= await Listing.find({});
-  res.render("listings/index.ejs", {allListings});
-
-});
-
-// New route 
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-  });
-  
-
-//Show route
-
-app.get("/listings/:id", async(req, res) => {
-    let {id} =req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-});
-
-// //Create route
-app.post("/listings",async (req, res) =>{
- 
-    const newListing = new Listing(req.body.listing);
-
-    // Save the new listing
-    await newListing.save();
-
-    // Redirect to the listings page upon successful creation
-    res.redirect("/listings");
-
-
-});
-
-// edit route
-
-app.get("/listings/:id/edit", async(req, res) =>{
-  let {id} =req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", {listing});
-});
-
-// update route 
-
-app.put("/listings/:id" , async(req, res) => {
-  let {id} =req.params;
-await Listing.findByIdAndUpdate(id, {...req.body.listing});
-res.redirect(`/listings/${id}`);
-  
-});
-
-// Delete route
-
-app.delete("/listings/:id" , async(req, res) => {
-  let {id} =req.params;
- let deletedListing = await  Listing.findByIdAndDelete(id);
- console.log(deletedListing);
- res.redirect("/listings");  
-});
 
 
 
-
-
-// app.get("/testListing" , async(req,res) => {
-//    let sampleListing = new Listing ({
-//       title: "My new villa",
-//       description: "By the Beach",
-//       price : 1200,
-//       location : "Calagunt , Goa",
-//       country : "India",
-//    });
-   
-//    await sampleListing.save();
-//    console.log("sample was saved");
-//    res.send("Successfull testing");
-   
-// });
-=======
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto :{
@@ -206,7 +115,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 
+
 
 
 
@@ -229,7 +138,7 @@ app.use((err, req, res, next) => {
  // res.status(statusCode).send(message);
   
 });
->>>>>>> c2fec04 (Added my project)
+
  
 
 app.listen(8080, () => {
